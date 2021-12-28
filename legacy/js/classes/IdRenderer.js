08b67c28592deb322de475249dccec6a68bca618
Przemysław Rogalski <przemyslaw.rogalski@goingapp.pl>
2021-12-28 23:40:05 +0100

    increase padding on top of the back

--- legacy/js/classes/IdRenderer.js
+++ legacy/js/classes/IdRenderer.js
@@ -54,7 +54,7 @@ const IdRenderer = function (competitionId) {
             </div>
         </div>
         <div class="id-card">
-            <div class="logo-bg" style="padding-top: 20px;">
+            <div class="logo-bg" style="padding-top: 30px;">
                 <p style="text-align: center; margin: 2px 0">${competitor.name}${ competitor.wcaId ? ', WCA ID: ' + competitor.wcaId : ''}</p>
                 ${this.createAssignementsSection(this.generator.getActivitiesForCompetitor(competitor.wcaId), competitor, lang)}
             </div>
